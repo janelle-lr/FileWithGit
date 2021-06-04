1d963ac78212cd34a0dd01cd735603a1b97345be
janelle-lr <janelle.limranola@gmail.com>
2021-06-04 23:27:49 +1200

    added in user input and output to txt file

--- FileWithGit.cpp
+++ FileWithGit.cpp
@@ -15,7 +15,51 @@ struct Jacket {
     float price = 0;
 };
 
+struct Jacket* inputJacketDetails(struct Jacket* j) {
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        cout << "Enter jacket brand : ";
+        getline(cin, j->brand);
+        cout << "Enter jacket material : ";
+        getline(cin, j->material);
+        cout << "Enter jacket size : ";
+        getline(cin, j->size);
+        cout << "Enter jacket color : ";
+        cin >> j->color;
+        cout << "Enter jacket price : ";
+        cin >> j->price;
+        cout << "\n";
+        cin.ignore();
+        j++;
+    }
+
+    return (j);
+}
+
 int main()
 {
+    struct Jacket jckt[5];
+    struct Jacket* j = jckt;
+    int i;
 
+    inputJacketDetails(j);
+
+    //writing to the file
+    fstream jacketFile;
+
+    //open the file
+    jacketFile.open("jacket.txt", ios::out);
+
+    //writing input data into file
+    for (i = 0; i < 5; i++) {
+        jacketFile << j->brand << "\t";
+        jacketFile << j->material << "\t";
+        jacketFile << j->size << "\t";
+        jacketFile << j->color << "\t";
+        jacketFile << j->price << endl;
+        j++;
+    }
+    cout << "\nStored details into the file" << endl;
+    jacketFile.close();
 }
