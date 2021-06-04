e667714d972576fb8e7bac2b4d7456e5bf56889f
janelle-lr <janelle.limranola@gmail.com>
2021-06-04 23:01:29 +1200

    first commit

---
+++ FileWithGit.cpp
@@ -0,0 +1,21 @@
+// FileWithGit.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct Jacket {
+    string brand;
+    string material;
+    string color;
+    string size;
+    float price = 0;
+};
+
+int main()
+{
+
+}
